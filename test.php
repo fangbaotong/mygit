89bd7aa136ed4408516be056524a5722b54ef0bb
fbt <976727420@qq.com>
2017-03-24 14:44:48 +0800

    add test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+/*git config --global user.name "fbt"
+%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;D:\web\mysql\bin\;D:\web\www\devtools\vendor\phalcon\devtools\;
+D:\web\www\devtools\vendor\phalcon\devtools\;*/
+
+echo 13;
+exit;
+echo 're';
+echo 000;
+echo 111;
